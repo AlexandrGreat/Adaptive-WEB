3d1832801a62e3e18f3dca903d66d04ecf14d0b1
AlexandrGreat <109344446+AlexandrGreat@users.noreply.github.com>
2024-02-27 15:52:23 +0200

    Update WeatherAPIController.cs

--- Controllers/WeatherAPIController.cs
+++ Controllers/WeatherAPIController.cs
@@ -18,19 +18,27 @@ namespace LR5.Controllers
             string url = $"http://api.openweathermap.org/data/2.5/weather?q=,&appid=a28598d19e603a2c376487760c42e48b&units=metric";
             var client = new HttpClient();
             var response = await client.GetAsync(url);
-
+            ResponseModel responseModel = new ResponseModel();
 
             try
             {
-                ResponseModel responseModel = new ResponseModel();
                 responseModel.status = (int)response.StatusCode;
                 responseModel.response = await response.Content.ReadAsStringAsync();
                 responseModel.headers = response.Headers.ToString();
-                Console.WriteLine(responseModel.ToString());
-            }catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
                 return StatusCode(500);
+            }
+            finally
+            {
+                Console.WriteLine("-----------------------");
+                ResponseModel.APIDataStorage.Add(responseModel.ToString());
+                foreach (var item in ResponseModel.APIDataStorage)
+                {
+                    Console.WriteLine(item);
+                }
             }
 
             return StatusCode((int)response.StatusCode, response.Content.ReadAsStringAsync());
@@ -42,19 +50,27 @@ namespace LR5.Controllers
             string url = $"http://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid=a28598d19e603a2c376487760c42e48b&units=metric";
             var client = new HttpClient();
             var response= await client.GetAsync(url);
+            ResponseModel responseModel = new ResponseModel();
 
             try
             {
-                ResponseModel responseModel = new ResponseModel();
                 responseModel.status = (int)response.StatusCode;
                 responseModel.response = await response.Content.ReadAsStringAsync();
                 responseModel.headers = response.Headers.ToString();
-                Console.WriteLine(responseModel.ToString());
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());  
                 return StatusCode(500);
+            }
+            finally
+            {
+                ResponseModel.APIDataStorage.Add(responseModel.ToString());
+                Console.WriteLine("-----------------------");
+                foreach (var item in ResponseModel.APIDataStorage)
+                {
+                    Console.WriteLine(item);
+                }
             }
 
             return StatusCode((int)response.StatusCode, response.Content.ReadAsStringAsync());
