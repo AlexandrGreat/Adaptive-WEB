46899a5d127f51191d8533fbc7c98a3ae961f8db
AlexandrGreat <109344446+AlexandrGreat@users.noreply.github.com>
2024-03-11 17:06:49 +0200

    Update RegistrationModel.cs

--- Models/RegistrationModel.cs
+++ Models/RegistrationModel.cs
@@ -1,9 +1,14 @@
-﻿namespace LR6.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace LR6.Models
 {
     public class RegistrationModel
     {
+        [MaxLength(15, ErrorMessage = "Maximum length 15")]
         public string Name { get; set; }
+        [MaxLength(15, ErrorMessage = "Maximum length 15")]
         public string Surname { get; set; }
+        [EmailAddress(ErrorMessage = "Invalid Email address")]
         public string Email { get; set; }
         public DateOnly BirthDate { get; set; }
         public string Password { get; set; }
